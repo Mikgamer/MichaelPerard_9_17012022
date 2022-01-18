de5df5390ac39fb1cb1edcaa32a5636f3716069e
Mikgamer <50680361+Mikgamer@users.noreply.github.com>
2022-01-18 10:46:12 +0100

    Vérification de l'existence de fiches avant de les trier

--- bill-app/Billed-app-FR-Front/src/views/BillsUI.js
+++ bill-app/Billed-app-FR-Front/src/views/BillsUI.js
@@ -20,7 +20,7 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  data.sort((a, b) => ((a.date < b.date) ? 1 : -1))
+  if (data !== undefined) { data.sort((a, b) => ((a.date < b.date) ? 1 : -1)) }
   return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
